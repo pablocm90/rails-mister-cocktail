ef362d5a99d03f6caf2503b323c64795ca5fd862
pablo curell <pablocm90@gmail.com>
2017-11-04 18:49:23 +0100

    changed seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,22 +11,8 @@ puts "deleting all things"
 Cocktail.destroy_all
 Dose.destroy_all
 Ingredient.destroy_all
+Review.destroy_all
 
-puts "creating cocktails"
-
-pictures = %w(http://res.cloudinary.com/pablocm90/image/upload/v1509715579/cocktail1.jpg http://res.cloudinary.com/pablocm90/image/upload/v1509715575/cocktail5.jpg http://res.cloudinary.com/pablocm90/image/upload/v1509715570/cocktail6.jpg http://res.cloudinary.com/pablocm90/image/upload/v1509715566/cocktail2.jpg http://res.cloudinary.com/pablocm90/image/upload/v1509715563/cocktail3.jpg http://res.cloudinary.com/pablocm90/image/upload/v1509715557/cocktail4.jpg)
-
-
-10.times do
-  url = pictures.sample
-  name = Faker::LordOfTheRings.character
-  cocktail = Cocktail.new(name: name)
-  cocktail.remote_photo_url = url
-  cocktail.save
-  p "#{cocktail.name}"
-end
-
-puts "creating ingredients"
 
 require 'json'
 require 'open-uri'
@@ -51,17 +37,3 @@ end
 
 puts "Finished seeding"
 
-
-puts "creating doses"
-
-30.times do
-  quantity = rand(1..10)
-  description = quantity.to_s + " " + Faker::Food.metric_measurement
-  p description
-  cocktail = Cocktail.all.sample
-  ingredient = Ingredient.all.sample
-  dose = Dose.new(description: description, cocktail: cocktail, ingredient: ingredient)
-  dose.save
-end
-
-puts "done"
